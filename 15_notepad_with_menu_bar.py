234d37487a6ff4471cd43485c77acaf859a2aa75
pocc <whim42@gmail.com>
2016-11-19 16:41:25 -0800

    text editor is working with menu bar

--- 15_notepad_with_menu_bar.py
+++ 15_notepad_with_menu_bar.py
@@ -1,15 +1,72 @@
 import sys, os
-from PyQt5.QtWidgets import QApplication, QMainWindow, QAction, qApp
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget
+from PyQt5.QtWidgets import QTextEdit, QPushButton, QVBoxLayout, QHBoxLayout, QAction, qApp
+
+import sys, os
+from PyQt5.QtWidgets import QApplication, QTextEdit, QWidget, QPushButton, QVBoxLayout, QHBoxLayout, QFileDialog
 
 
-class MenuDemo(QMainWindow):
+
+# This program will save a file (first text box) and throw out text (second text box)
+class Notepad(QWidget):
+
+    def __init__(self):
+        super(Notepad, self).__init__()
+        self.text = QTextEdit(self)
+        self.clr_btn = QPushButton('Clear')
+        self.sav_btn = QPushButton('Save')
+        self.opn_btn = QPushButton('Open')
+
+        self.init_ui()
+
+    def init_ui(self):
+        v_layout = QVBoxLayout()
+        h_layout = QHBoxLayout()
+
+        h_layout.addWidget(self.clr_btn)
+        h_layout.addWidget(self.sav_btn)
+        h_layout.addWidget(self.opn_btn)
+
+        v_layout.addWidget(self.text)
+        v_layout.addLayout(h_layout)
+
+        self.sav_btn.clicked.connect(self.save_text)
+        self.clr_btn.clicked.connect(self.clear_text)
+        self.opn_btn.clicked.connect(self.open_text)
+
+        self.setLayout(v_layout)
+        self.setWindowTitle('PyQt5 TextEdit')
+
+        self.show()
+
+    def save_text(self):
+        # QT way
+        filename = QFileDialog.getSaveFileName(self, 'Save File', os.getenv('HOME'))
+        with open(filename[0], 'w') as f:
+            my_text = self.text.toPlainText()
+            f.write(my_text)
+
+    def clear_text(self):
+        self.text.clear()
+
+    def open_text(self):
+        filename = QFileDialog.getOpenFileName(self, 'Open File', os.getenv('HOME'))
+        with open(filename[0], 'r') as f:
+            file_text = f.read()
+            self.text.setText(file_text)
+
+
+class Writer(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        # Create Menu Bar
-        bar = self.menuBar()
+        self.form_widget = Notepad()
+        self.setCentralWidget(self.form_widget)
 
-        # Create Root Menus
+        self.init_ui()
+
+    def init_ui(self):
+        bar = self.menuBar()
         file = bar.addMenu('File')
         edit = bar.addMenu('Edit')
 
@@ -20,7 +77,7 @@ class MenuDemo(QMainWindow):
         new_action = QAction('New', self)
         new_action.setShortcut(('Ctrl+N'))
 
-        quit_action = QAction('Quit', self)
+        quit_action = QAction('&Quit', self)
         quit_action.setShortcut('Ctrl+Q')
 
         find_action = QAction('Find...', self)
@@ -35,22 +92,22 @@ class MenuDemo(QMainWindow):
         find_menu.addAction(find_action)
         find_menu.addAction(replace_action)
 
-        # Events
         quit_action.triggered.connect(self.quit_trigger)
-        file.triggered.connect(self.selected)
-        edit.triggered.connect(self.selected)
-
-        self.setWindowTitle("My Menus")
-        self.resize(600, 400)
+        file.triggered.connect(self.respond)
 
         self.show()
-
-    def quit_trigger(self):
+    def quit_trigger(selfself):
         qApp.quit()
 
-    def selected(self, q):
-        print(q.text() + ' selected')
+    def respond(self, q):
+        signal = q.text()
+        if signal == 'New':
+            self.form_widget.clear_text()
+        elif signal == '&Open':
+            self.form_widget.open_text()
+        elif signal == '&Save':
+            self.form_widget.save_text()
 
 app = QApplication(sys.argv)
-menus = MenuDemo()
+writer = Writer()
 sys.exit(app.exec_())
